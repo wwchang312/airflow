7e9133c7bf4d6f74d8f87986a0586a6f7dc774c3
wwchang <wanw95@gmail.com>
2025-07-08 18:17:59 +0900

    함수 오타 수정

--- plugins/common/common_func.py
+++ plugins/common/common_func.py
@@ -5,13 +5,13 @@ def get_sftp():
 def regist(name, sex, *args):
     print(f'이름: {name}')
     print(f'성별: {sex}')
-    print(f'기타옵션': {args})
+    print(f'기타옵션: {args}')
 
 
-def regist2(nema,sex,*args,**kwargs):
+def regist2(name,sex,*args,**kwargs):
     print(f'이름: {name}')
     print(f'성별: {sex}')
-    print(f'기타옵션': {args})
+    print(f'기타옵션: {args}')
     email = kwargs['email'] or None
     phone = kwargs['phone'] or None
     if email:
