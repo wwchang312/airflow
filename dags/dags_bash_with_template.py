3804583bd36ee89e38219a4901cbce6db598dd3e
wwchang <wanw95@gmail.com>
2025-07-09 17:22:08 +0900

    dag_id 변경 및 스케줄 파라미터 변경

--- dags/dags_bash_with_template.py
+++ dags/dags_bash_with_template.py
@@ -4,8 +4,8 @@ import datetime
 from airflow.providers.standard.operators.bash import BashOperator
 
 with DAG(
-    dag_id ="dags_email_operator",
-    schedule= "0 8 1 * *",
+    dag_id ="dags_bash_with_template",
+    schedule= "10 0 * * *",
     start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup= False
 ) as dag:
