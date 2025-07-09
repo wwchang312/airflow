d051d9ffe5107bb13cd4ec63bcfb1f3810e81244
wwchang <wanw95@gmail.com>
2025-07-09 17:15:29 +0900

    오타수정

--- dags/dags_bash_with_template.py
+++ dags/dags_bash_with_template.py
@@ -20,7 +20,7 @@ with DAG(
             'START_DATE':'{{data_interval_start | ds}}',
             'END_DATE':'{{data_interval_end | ds }}'
         },
-        bash_commnad = 'echo $START_DATE && echo $END_DATE'
+        bash_command = 'echo $START_DATE && echo $END_DATE'
     )
     
     bash_t1 >> bash_t2
