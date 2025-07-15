2561467c97aa39c1b6b34691dcd14e06cc38ccb7
wwchang <wanw95@gmail.com>
2025-07-15 15:49:27 +0900

    파이썬 탬플릿 실습

---
+++ dags/dag_python_template.py
@@ -0,0 +1,31 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.decorators import task
+from airflow.providers.standard.operators.python import PythonOperator
+
+with DAG(
+    dag_id='dag_python_template',
+    schedule= '30 9 * * *',
+    start_date= pendulum.datetime(2025,7,1),
+    catchup=False
+) as dag:
+
+    def python_function1(start_date,end_date,**kwargs):
+        print(start_date)
+        print(end_date)
+
+    python_t1 = PythonOperator(
+        task_id ="python_t1",
+        python_callable=python_function1,
+        op_kwargs={'start_date':'{{data_interval_start |ds }}','end_date' :'{{data_interval_end | ds}}'}
+    )
+
+    @task(task_id = 'python_t2')
+    def python_function2(**kwargs):
+        print(kwargs)
+        print('ds:' + kwargs['ds'])
+        print('ts:' + kwargs['ts'])
+        print('data_interval_start:' + str(kwargs['data_interval_start']))
+        print('data_interval_end:' + str(kwargs['data_interval_end']))
+        print('task_instance:' + str(kwargs['ti']))
