098a66acbba8f6bacb9faad294dec506538a40ce
wwchang <wanw95@gmail.com>
2025-07-17 21:15:17 +0900

    bash오퍼레이터 xcom

---
+++ dags/dags_bash_with_xcom.py
@@ -0,0 +1,30 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.providers.standard.operators.bash import BashOperator
+
+with DAG(
+    dag_id ="dags_bash_select_fruit",
+    schedule= "10 0 * * 6#1",
+    start_date=pendulum.datetime(2025, 7, 1, tz="Asia/Seoul"),
+    catchup= False
+) as dag:
+
+    bash_push = BashOperator(
+        task_id='bash_push',
+        bash_commnad="echo START &&"
+                     "echo XCOM_PUSHED "
+                     "{{ti.xcom_push(key='bash_pushed',value='first_bash_message' }} && "
+                     "echo COMPLETE"
+    )
+
+    bash_pull = BashOperator(
+        task_id='bash_pull',
+        env={'PUSHED_VALUE':"{{ti.xcom_pull(key='bash_pushed',task_id='bash_push')}}",
+             'RETURN_VALUE':"{{ti.xcom_pull(task_id='bash_push')}}"},
+        bash_command = "echo $PUSHED_VALUE && echo RETURN_VALUE",
+        do_xcom_push=False
+    )
+
+
+    bash_push >> bash_pull
