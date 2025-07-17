007ba53f3aa7b6f8f97c3eab2da770e143761b14
wwchang <wanw95@gmail.com>
2025-07-17 19:31:35 +0900

    파이썬 오퍼레이터 xcom 실습

---
+++ dags/dags_python_with_xcom_eg2.py
@@ -0,0 +1,32 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.decorators import task
+
+with DAG(
+    dag_id='dags_python_with_xcom_eg1',
+    schedule= '0 10 * * *',
+    start_date= pendulum.datetime(2025,7,1),
+    catchup=False
+) as dag:
+
+    @task(task_id='python_xcom_push_by_return')
+    def xcom_push_result(**kwargs):
+        return "Success"
+
+
+    @task(task_id='python_xcom_pull_1')
+    def xcom_pull_1(**kwargs):
+        ti = kwargs['ti']
+        value1= ti.xcom_pull(task_ids='python_xcom_push_by_return')
+        print('xcom_pull 메서드로 직접 찾은 리턴 값 : ' + value1)
+    
+    @task(task_id='python_xcom_pull_2')
+    def xcom_pull_2(status,**kwargs):
+        print('함수 입력값으로 받은 값 : ' + status)
+    
+
+    xcom_pull2(xcom_push_result())
+    xcom_push_result() >> xcom_pull1()
+    
+    
