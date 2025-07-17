b538a9eb39185e243859b328ad41bbe891fcd99d
wwchang <wanw95@gmail.com>
2025-07-17 22:19:21 +0900

    오타수정

--- dags/dags_bash_with_xcom.py
+++ dags/dags_bash_with_xcom.py
@@ -20,8 +20,8 @@ with DAG(
 
     bash_pull = BashOperator(
         task_id='bash_pull',
-        env={'PUSHED_VALUE':"{{ti.xcom_pull(key='bash_pushed',task_id='bash_push')}}",
-             'RETURN_VALUE':"{{ti.xcom_pull(task_id='bash_push')}}"},
+        env={'PUSHED_VALUE':"{{ti.xcom_pull(key='bash_pushed',task_ids='bash_push')}}",
+             'RETURN_VALUE':"{{ti.xcom_pull(task_ids='bash_push')}}"},
         bash_command = "echo $PUSHED_VALUE && echo RETURN_VALUE",
         do_xcom_push=False
     )
