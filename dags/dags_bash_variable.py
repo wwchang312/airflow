ee27117dc6bea8083f98397d153ffa8599f27575
wwchang <wanw95@gmail.com>
2025-07-25 01:47:22 +0900

    varialble 라이브러리 수정

--- dags/dags_bash_variable.py
+++ dags/dags_bash_variable.py
@@ -14,7 +14,7 @@ with DAG(
 
     bash_var_1 = BashOperator(
         task_id = 'bash_var_1',
-        bash_command=f"echo variable:{{var_value}}"
+        bash_command=f"echo variable:{var_value}"
     )
 
     bash_var_2=BashOperator(
