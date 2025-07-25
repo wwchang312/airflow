842635e849c54e8e8cedc0bb2b2acdb5996a61f1
wwchang <wanw95@gmail.com>
2025-07-25 22:48:50 +0900

    오타 수정

--- dags/dags_python_with_branch_decorator.py
+++ dags/dags_python_with_branch_decorator.py
@@ -19,31 +19,33 @@ with DAG(
         elif selected_item in ['B','C']:
             return ['task_b','task_c']
         
-        python_branch_task = BranchPythonOperator(
-            task_id ='python_branch_task',
-            python_callable = select_random
-        )
+    python_branch_task = BranchPythonOperator(
+        task_id ='python_branch_task',
+        python_callable = select_random
+    )
 
 
-        def common_func(**kwargs):
-            print(kwargs['selected'])
+    def common_func(**kwargs):
+        print(kwargs['selected'])
 
-        task_a= PythonOperator(
-            task_id='task_a',
-            python_callable=common_func,
-            op_kwargs={'selected' : 'A'}
-        )
+    task_a= PythonOperator(
+        task_id='task_a',
+        python_callable=common_func,
+        op_kwargs={'selected' : 'A'}
+    )
 
-        task_b=PythonOperator(
-            task_id='task_b',
-            python_callable=common_func,
-            op_kwargs={'selected' : 'B'}
-        )
+    task_b=PythonOperator(
+        task_id='task_b',
+        python_callable=common_func,
+        op_kwargs={'selected' : 'B'}
+    )
 
-        task_c=PythonOperator(
-            task_id='task_c',
-            python_callable=common_func,
-            op_kwargs={'selected' : 'C'}
-        )
+    task_c=PythonOperator(
+        task_id='task_c',
+        python_callable=common_func,
+        op_kwargs={'selected' : 'C'}
+    )
 
-    python_branch_task >> [task_a,task_b,task_c] # type: ignore
+    python_branch_task >> [task_a,task_b,task_c]
+
+    
