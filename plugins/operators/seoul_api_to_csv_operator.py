1766ac6db5df00380d631d9b214abe871e4f2572
wwchang <wanw95@gmail.com>
2025-07-30 22:52:06 +0900

    탬플릿 필드 수정

--- plugins/operators/seoul_api_to_csv_operator.py
+++ plugins/operators/seoul_api_to_csv_operator.py
@@ -3,14 +3,14 @@ from airflow.hooks.base import BaseHook
 import pandas as pd 
 
 class SeoulApiToCsvOperator(BaseOperator):
-    templated_fields = ('endpoint','path','file_name','base_dt')
+    template_fields = ('endpoint','path','file_name','base_dt')
     
     def __init__(self, dataset_nm,path,file_name,base_dt=None, **kwargs):
         super().__init__(**kwargs)
         self.http_conn_id = 'openapi.seoul.go.kr'
         self.path = path
         self.file_name = file_name
-        self.endpoint = '{{var.value.apikey_openapi_seoul_go_kr}}/json/' + dataset_nm
+        self.endpoint = "{{var.value.apikey_openapi_seoul_go_kr}}/json/" + dataset_nm
         self.base_dt = base_dt
 
 
